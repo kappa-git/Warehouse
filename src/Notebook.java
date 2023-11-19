66ab44935469ce7dfa84b80d7a4eccc342b0c7f3
carmentecchia <carmentecchia@gmail.com>
2023-11-19 17:09:23 +0100

    Ricerca per Produttore

--- src/Notebook.java
+++
@@ -1,9 +0,0 @@
-public class Notebook extends Prodotto {
-    public Notebook(String produttore, String modello, String descrizione,
-                    double dimensioneDisplay, double dimensioneArchiviazione,
-                    double prezzoAcquisto, double prezzoVendita, int id) {
-        super("Notebook", produttore, modello, descrizione, dimensioneDisplay,
-                dimensioneArchiviazione, prezzoAcquisto, prezzoVendita, id);
-    }
-
-}
