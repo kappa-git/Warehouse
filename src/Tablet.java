33ab25dee072ec9b46f24db89b9c9c63a96617ae
Federico Maiello <federicomaiello@hotmail.it>
2023-11-23 15:22:18 +0100

    Tablet

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,5 +1,97 @@
-public class Tablet{
-    public static void main(String[] args) {
-       int numero = 2;
+public class Tablet {
+    private String deviceType;
+    private String manufacturer;
+    private String model;
+    private String description;
+    private double displaySize;
+    private double storageSize;
+    private double purchasePrice;
+    private double sellingPrice;
+    private int deviceId;
+
+    // Constructor
+    public Tablet(String deviceType, String manufacturer, String model, String description,
+                  double displaySize, double storageSize, double purchasePrice, double sellingPrice, int deviceId) {
+        this.deviceType = deviceType;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.description = description;
+        this.displaySize = displaySize;
+        this.storageSize = storageSize;
+        this.purchasePrice = purchasePrice;
+        this.sellingPrice = sellingPrice;
+        this.deviceId = deviceId;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getDisplaySize() {
+        return displaySize;
+    }
+
+    public void setDisplaySize(double displaySize) {
+        this.displaySize = displaySize;
+    }
+
+    public double getStorageSize() {
+        return storageSize;
+    }
+
+    public void setStorageSize(double storageSize) {
+        this.storageSize = storageSize;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int deviceId) {
+        this.deviceId = deviceId;
     }
 }
