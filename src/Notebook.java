d71ef6045270ecc20f9d48ef90ea52404c18b953
kappa-git <kevin.borgia@outlook.it>
2023-11-30 19:01:03 +0100

    Classe notebook

--- src/Notebook.java
+++ src/Notebook.java
@@ -97,16 +97,15 @@ public class Notebook {
 
     @Override
     public String toString() {
-        return "Notebook{" +
-                "deviceType='" + deviceType + '\'' +
-                ", manufacturer='" + manufacturer + '\'' +
-                ", model='" + model + '\'' +
-                ", description='" + description + '\'' +
-                ", displaySize=" + displaySize +
-                ", storageSize=" + storageSize +
-                ", purchasePrice=" + purchasePrice +
-                ", sellingPrice=" + sellingPrice +
-                ", deviceId=" + deviceId +
-                '}';
+        return '\n' + "-------------------------- " + '\n' +
+                "- deviceType = " + deviceType  + '\n' +
+                "- manufacturer = " + manufacturer + '\n' +
+                "- model = " + model + '\n' +
+                "- description = " + description + '\n' +
+                "- displaySize = " + displaySize + '\n' +
+                "- storageSize = " + storageSize + '\n' +
+                "- purchasePrice = " + purchasePrice + '\n' +
+                "- sellingPrice = " + sellingPrice + '\n' +
+                "- deviceId=" + deviceId + '\n';
     }
 }
