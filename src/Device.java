fbda9027ab9212307ae242d245e5cae547b8c651
Federico Maiello <federicomaiello@hotmail.it>
2023-12-01 14:30:14 +0100

    Aggiornamento warehouse e creazione file Device.java

---
+++ src/Device.java
@@ -0,0 +1,13 @@
+public interface Device {
+    String getDeviceType();
+    String getManufacturer();
+    String getModel();
+    String getDescription();
+    double getDisplaySize();
+    int getStorageSize();
+    double getPurchasePrice();
+    double getSellingPrice();
+    int getDeviceId();
+    String toString();
+
+}
