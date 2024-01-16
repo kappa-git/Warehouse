033830b4a388d7284a09032c1386c0fea8ed076f
kappa-git <kevin.borgia@outlook.it>
2024-01-16 10:17:21 +0100

    Refresh branch test

--- src/WarehouseManagerTest.java
+++ src/WarehouseManagerTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseManagerTest {
-    @Test
+    @Test // Controllo se il metodo searchPurchasePrice è valido
     void checkIfSearchPurchasePriceIsValid(){
         Double price = null;
         WarehouseManager warehouseManager = new WarehouseManager();
@@ -11,14 +15,22 @@ class WarehouseManagerTest {
         /*Il metodo si aspetta un primitivo e noi gli stiamo passando null*/
     }
 
-    @Test
+    @Test // Controllo che il metodo searchPurchasePrice non ritorna un null
     void checkIfSearchPurchasePriceIsNotRetrurnNull(){
-        Double input = 3.0;
+        double input = new Random().nextDouble();
         WarehouseManager warehouseManager = new WarehouseManager();
         assertNotEquals(warehouseManager.searchByPurchasePrice(input), null);
         /* Cosa stiamo testando?
         * Passando come input un valore che non sia null, vogliamo testare se il
         * metodo restituisce una Lista che non sia null*/
     }
+    @Test
+    void checkIfSearchPurchasePriceIsNegative(){
+        double input = -1;
+        WarehouseManager warehouseManager = new WarehouseManager();
+        List<Product> result = warehouseManager.searchByPurchasePrice(input);
+        assertEquals(result, Collections.emptyList());
+    }
+    
 
 }
